3d9459cac84e81fccda66e30c8a147aae013fecb
ningyu1 <ningbe111@163.com>
2018-06-20 15:41:37 +0800

    增加数组类型参数支持，具体查看参数对照表

--- src/main/java/io/github/ningyu/jmeter/plugin/util/ClassUtils.java
+++ src/main/java/io/github/ningyu/jmeter/plugin/util/ClassUtils.java
@@ -77,68 +77,134 @@ public class ClassUtils {
 		if (className.equals("int")) {
 			paramterTypeList.add(arg.getParamType());
 			parameterValuesList.add(Integer.parseInt(arg.getParamValue()));
+		} else if(className.equals("int[]")) {
+			paramterTypeList.add(arg.getParamType());
+			parameterValuesList.add(JsonUtils.formJson(arg.getParamValue(), new TypeToken<int[]>() {}.getType()));
 		} else if (className.equals("double")) {
 			paramterTypeList.add(arg.getParamType());
 			parameterValuesList.add(Double.parseDouble(arg.getParamValue()));
+		}  else if (className.equals("double[]")) {
+			paramterTypeList.add(arg.getParamType());
+			parameterValuesList.add(JsonUtils.formJson(arg.getParamValue(), new TypeToken<double[]>() {}.getType()));
 		} else if (className.equals("short")) {
 			paramterTypeList.add(arg.getParamType());
 			parameterValuesList.add(Short.parseShort(arg.getParamValue()));
+		} else if (className.equals("short[]")) {
+			paramterTypeList.add(arg.getParamType());
+			parameterValuesList.add(JsonUtils.formJson(arg.getParamValue(), new TypeToken<short[]>() {}.getType()));
 		} else if (className.equals("float")) {
 			paramterTypeList.add(arg.getParamType());
 			parameterValuesList.add(Float.parseFloat(arg.getParamValue()));
+		} else if (className.equals("float[]")) {
+			paramterTypeList.add(arg.getParamType());
+			parameterValuesList.add(JsonUtils.formJson(arg.getParamValue(), new TypeToken<float[]>() {}.getType()));
 		} else if (className.equals("long")) {
 			paramterTypeList.add(arg.getParamType());
 			parameterValuesList.add(Long.parseLong(arg.getParamValue()));
+		} else if (className.equals("long[]")) {
+			paramterTypeList.add(arg.getParamType());
+			parameterValuesList.add(JsonUtils.formJson(arg.getParamValue(), new TypeToken<long[]>() {}.getType()));
 		} else if (className.equals("byte")) {
 			paramterTypeList.add(arg.getParamType());
 			parameterValuesList.add(Byte.parseByte(arg.getParamValue()));
+		} else if (className.equals("byte[]")) {
+			paramterTypeList.add(arg.getParamType());
+			parameterValuesList.add(JsonUtils.formJson(arg.getParamValue(), new TypeToken<byte[]>() {}.getType()));
 		} else if (className.equals("boolean")) {
 			paramterTypeList.add(arg.getParamType());
 			parameterValuesList.add(Boolean.parseBoolean(arg.getParamValue()));
+		} else if (className.equals("boolean[]")) {
+			paramterTypeList.add(arg.getParamType());
+			parameterValuesList.add(JsonUtils.formJson(arg.getParamValue(), new TypeToken<boolean[]>() {}.getType()));
 		} else if (className.equals("char")) {
 			paramterTypeList.add(arg.getParamType());
 			parameterValuesList.add(arg.getParamValue().charAt(0));
+		} else if (className.equals("char[]")) {
+			paramterTypeList.add(arg.getParamType());
+			parameterValuesList.add(JsonUtils.formJson(arg.getParamValue(), new TypeToken<char[]>() {}.getType()));
 		} else if (className.equals("java.lang.String")
-				|| className.equals("String") || className.equals("string")) {
+				|| className.equals("String") 
+				|| className.equals("string")) {
 			paramterTypeList.add("java.lang.String");
 			parameterValuesList.add(String.valueOf(arg.getParamValue()));
+		} else if (className.equals("java.lang.String[]")
+				|| className.equals("String[]") 
+				|| className.equals("string[]")) {
+			paramterTypeList.add("java.lang.String[]");
+			parameterValuesList.add(JsonUtils.formJson(arg.getParamValue(), new TypeToken<String[]>() {}.getType()));
 		} else if (className.equals("java.lang.Integer")
-				|| className.equals("Integer") || className.equals("integer")) {
+				|| className.equals("Integer") 
+				|| className.equals("integer")) {
 			paramterTypeList.add("java.lang.Integer");
 			parameterValuesList.add(Integer.valueOf(arg.getParamValue()));
+		} else if (className.equals("java.lang.Integer[]")
+				|| className.equals("Integer[]") 
+				|| className.equals("integer[]")) {
+			paramterTypeList.add("java.lang.Integer[]");
+			parameterValuesList.add(JsonUtils.formJson(arg.getParamValue(), new TypeToken<Integer[]>() {}.getType()));
 		} else if (className.equals("java.lang.Double")
 				|| className.equals("Double")) {
 			paramterTypeList.add("java.lang.Double");
 			parameterValuesList.add(Double.valueOf(arg.getParamValue()));
+		} else if (className.equals("java.lang.Double[]")
+				|| className.equals("Double[]")) {
+			paramterTypeList.add("java.lang.Double[]");
+			parameterValuesList.add(JsonUtils.formJson(arg.getParamValue(), new TypeToken<Double[]>() {}.getType()));
 		} else if (className.equals("java.lang.Short")
 				|| className.equals("Short")) {
 			paramterTypeList.add("java.lang.Short");
 			parameterValuesList.add(Short.valueOf(arg.getParamValue()));
+		} else if (className.equals("java.lang.Short[]")
+				|| className.equals("Short[]")) {
+			paramterTypeList.add("java.lang.Short[]");
+			parameterValuesList.add(JsonUtils.formJson(arg.getParamValue(), new TypeToken<Short[]>() {}.getType()));
 		} else if (className.equals("java.lang.Long")
 				|| className.equals("Long")) {
 			paramterTypeList.add("java.lang.Long");
 			parameterValuesList.add(Long.valueOf(arg.getParamValue()));
+		} else if(className.equals("java.lang.Long[]")
+				|| className.equals("Long[]")) {
+			paramterTypeList.add("java.lang.Long[]");
+			parameterValuesList.add(JsonUtils.formJson(arg.getParamValue(), new TypeToken<Long[]>() {}.getType()));
 		} else if (className.equals("java.lang.Float")
 				|| className.equals("Float")) {
 			paramterTypeList.add("java.lang.Float");
 			parameterValuesList.add(Float.valueOf(arg.getParamValue()));
+		} else if (className.equals("java.lang.Float[]")
+				|| className.equals("Float[]")) {
+			paramterTypeList.add("java.lang.Float[]");
+			parameterValuesList.add(JsonUtils.formJson(arg.getParamValue(), new TypeToken<Float[]>() {}.getType()));
 		} else if (className.equals("java.lang.Byte")
 				|| className.equals("Byte")) {
 			paramterTypeList.add("java.lang.Byte");
 			parameterValuesList.add(Byte.valueOf(arg.getParamValue()));
+		} else if (className.equals("java.lang.Byte[]")
+				|| className.equals("Byte[]")) {
+			paramterTypeList.add("java.lang.Byte[]");
+			parameterValuesList.add(JsonUtils.formJson(arg.getParamValue(), new TypeToken<Byte[]>() {}.getType()));
 		} else if (className.equals("java.lang.Boolean")
 				|| className.equals("Boolean")) {
 			paramterTypeList.add("java.lang.Boolean");
 			parameterValuesList.add(Boolean.valueOf(arg.getParamValue()));
+		} else if (className.equals("java.lang.Boolean[]")
+				|| className.equals("Boolean[]")) {
+			paramterTypeList.add("java.lang.Boolean[]");
+			parameterValuesList.add(JsonUtils.formJson(arg.getParamValue(), new TypeToken<Boolean[]>() {}.getType()));
 		} else {
-			try {
-				Class<?> clazz = Class.forName(className);
+			if (className.endsWith("[]")) {
+				List<?> list = JsonUtils.formJson(arg.getParamValue(), new TypeToken<List<?>>() {}.getType());
 				paramterTypeList.add(arg.getParamType());
-				parameterValuesList.add(JsonUtils.formJson(arg.getParamValue(), clazz));
-			} catch (ClassNotFoundException e) {
-				//不是jdk或者lib下的类，使用通用map格式反序列化值
-				paramterTypeList.add(arg.getParamType());
-				parameterValuesList.add(JsonUtils.formJson(arg.getParamValue(), new TypeToken<Map<String,Object>>() {}.getType()));
+				parameterValuesList.add(list.toArray());
+			} else {
+				try {
+					Class<?> clazz = Class.forName(className);
+					paramterTypeList.add(arg.getParamType());
+					parameterValuesList.add(JsonUtils.formJson(arg.getParamValue(), clazz));
+				} catch (ClassNotFoundException e) {
+					//不是jdk或者lib下的类，使用通用map格式反序列化值
+					paramterTypeList.add(arg.getParamType());
+					parameterValuesList.add(JsonUtils.formJson(arg.getParamValue(), new TypeToken<Map<String,Object>>() {}.getType()));
+				}
 			}
 		}
 	}
